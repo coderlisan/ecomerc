077e9d12ec33eae351f68bc94c8cf6b429f1933b
coderlisan <enginerlisan@gmail.com>
2022-04-15 19:54:50 +0600

    router setup done

---
+++ src/Router.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import SingleProduct from './pages/SingleProduct';
+import Checkout from './pages/Checkout';
+import ShoppingCart from './pages/ShoppingCart';
+import Accounts from './pages/Accounts';
+import ComparePage from './pages/Compare';
+import MyShopPage from './pages/Shop';
+import WishlistPage from './pages/WishlistPage';
+
+const Router = () => {
+  return (
+        <Routes>
+
+          <Route path='/' element={ <Home/> } />
+          <Route path='/single-product' element = { <SingleProduct/>} />
+          <Route path='/checkout' element = { <Checkout/>} />
+          <Route path='/shopping-cart' element = { <ShoppingCart/>} />
+          <Route path='/my-account' element = { <Accounts/>} />
+          <Route path='/compare' element = { <ComparePage/>} />
+          <Route path='/shop' element = { <MyShopPage/>} />
+          <Route path='/wishlist' element = { <WishlistPage/>} />
+
+        </Routes>
+  );
+}
+ 
+export default Router;
