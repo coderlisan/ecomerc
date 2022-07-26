c1e9f9682c7898926ffaf926facb3890261d5842
coderlisan <enginerlisan@gmail.com>
2022-07-26 21:23:49 +0600

    I did very brain storming code on cart page

--- src/components/ShoppingCart/index.js
+++ src/components/ShoppingCart/index.js
@@ -2,8 +2,11 @@ import React, { useState } from 'react';
 import CartList from '../../data/CartLists.json';
          
 const ShoppingCart = () => {
-
     const [ cartList, setCartList ] = useState(CartList)
+    const [ coupon, setCoupon ] = useState("")
+    const [ discount, setDiscount ] = useState(0)
+        
+    let subTotal = 0
 
     const increaseQuantity = (id) => {
         cartList.forEach(item => {
@@ -14,7 +17,6 @@ const ShoppingCart = () => {
         })
       };
 
-
     const decreaseQuantity = (id) => {
         cartList.forEach(item => {
             if(item.id == id){
@@ -24,7 +26,26 @@ const ShoppingCart = () => {
         })
       };
 
+    cartList.forEach(item => {
+        subTotal = subTotal + (item.price * item.quantity)
+    })
 
+    const removeItem = (id) => {
+        const newCarts = cartList.filter(item => item.id !== id)
+        setCartList([...newCarts])
+    }
+    
+    const promoCodeValue = (event) => {
+        setCoupon(event.target.value)
+    }
+    
+    const discoundPrice = () => {
+        if (coupon == "OFF25"){
+            setDiscount(subTotal / 4)
+        }
+    }
+    
+    
     return (
         <React.Fragment>
             <div className="Shopping-cart-area pt-60 pb-60">
@@ -46,17 +67,17 @@ const ShoppingCart = () => {
                                         </thead>
                                         <tbody>
 
-                                        { cartList && typeof(cartList) == 'object' ? cartList.map((item, index) => {
+                                        { cartList ? cartList.map((item) => {
                                             return (
                                             <tr key={item.id}>
-                                                <td className="li-product-remove"><a href="#"><i className="fa fa-times"></i></a></td>
+                                                <td className="li-product-remove"><a onClick={() => removeItem(item.id)} href="#"><i className="fa fa-times"></i></a></td>
                                                 <td className="li-product-thumbnail"><a href="#"><img src={item.image_url} alt={item.title} /></a></td>
                                                 <td className="li-product-name"><a href="#">{item.title}</a></td>
                                                 <td className="li-product-price"><span className="amount">${item.price}</span></td>
                                                 <td className="quantity">
                                                     <label>Quantity</label>
                                                     <div className="cart-plus-minus">
-                                                        <input className="cart-plus-minus-box" value={item.quantity} onChange={() => console.log()} type="text" />
+                                                        <input className="cart-plus-minus-box" value={item.quantity} onChange={() => ""} type="text" />
                                                         <div onClick={() => decreaseQuantity(item.id)} className="dec qtybutton"><i className="fa fa-angle-down"></i></div>
                                                         <div onClick={() => increaseQuantity(item.id)} className="inc qtybutton"><i className="fa fa-angle-up"></i></div>
                                                     </div>
@@ -72,8 +93,8 @@ const ShoppingCart = () => {
                                     <div className="col-12">
                                         <div className="coupon-all">
                                             <div className="coupon">
-                                                <input id="coupon_code" className="input-text" name="coupon_code" defaultValue={""} placeholder="Coupon code" type="text" />
-                                                <input className="button" name="apply_coupon" defaultValue={"Apply coupon"} type="submit" />
+                                                <input id="coupon_code" className="input-text" name="coupon_code" value={coupon} onChange={promoCodeValue} placeholder="Coupon code" type="text" />
+                                                <input className="button" name="apply_coupon" onClick={discoundPrice} defaultValue={"Apply coupon"} type="submit" />
                                             </div>
                                             <div className="coupon2">
                                                 <input className="button" name="update_cart" value="Update cart" type="submit" />
@@ -86,8 +107,9 @@ const ShoppingCart = () => {
                                         <div className="cart-page-total">
                                             <h2>Cart totals</h2>
                                             <ul>
-                                                <li>Subtotal <span>$130.00</span></li>
-                                                <li>Total <span>$130.00</span></li>
+                                                <li>Subtotal <span>${subTotal}</span></li>
+                                                <li>Discount <span>${discount}</span></li>
+                                                <li>Total <span>${subTotal - discount}</span></li>
                                             </ul>
                                             <a href="#">Proceed to checkout</a>
                                         </div>
